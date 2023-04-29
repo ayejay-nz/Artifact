30cd93d3b390b7dae0bfdc22d625e03d13bec52a
ayejay <aidanjw05@gmail.com>
2023-04-29 20:04:20 +1200

    Setup apollo server and express app

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,30 @@ import { MikroORM, RequiredEntityData } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false,
+        }),
+    });
+
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(4000, () => {
+        console.log('server has started on localhost:4000');
+    });
 
     const emFork = orm.em.fork();
     const post = emFork.create(Post, {
