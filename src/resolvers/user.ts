14e580562280ce047f8cb3c810471131027b7d77
ayejay <aidanjw05@gmail.com>
2023-04-30 17:56:33 +1200

    Improve User resolver error handling
    - Adds FieldError class for better error handling and messages
    - Adds UserResponse class for handling users and if there was an error during information validation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,35 +1,96 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import bcrypt from 'bcrypt';
 import { RequiredEntityData } from "@mikro-orm/core";
 
 const hashingRounds = 12;
+const minUsernameLength = 3;
+const minPasswordLength = 10;
 
 @InputType()
 class UsernamePasswordInput {
     @Field()
-    username: string
+    username: string;
+
     @Field()
-    password: string
+    password: string;
+}
+
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+
+    @Field()
+    message:string;
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError[];
+
+    @Field(() => User, {nullable: true})
+    user?: User;
 }
 
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput,
         @Ctx() { em }: MyContext
-    ) {
+    ): Promise<UserResponse> {
+        if (options.username.length < minUsernameLength) {
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message: `length must be at least ${minUsernameLength}`
+                    },
+                ],
+            };
+        }
+
+        if (options.password.length < minPasswordLength) {
+            return {
+                errors: [
+                    {
+                        field: 'password',
+                        message: `length must be at least ${minPasswordLength}`
+                    },
+                ],
+            };
+        }
+
         const hashedPassword = await bcrypt.hash(options.password, hashingRounds);
 
         const user = em.create(User, { 
             username: options.username,
             password: hashedPassword
         } as RequiredEntityData<User>);
-        
-        await em.persistAndFlush(user);
 
-        return user;
+        try {
+            await em.persistAndFlush(user);
+        }
+        catch (err) {
+            // duplicate username error
+            if (err.code === '23505') {
+                return {
+                    errors: [
+                        {
+                            field: 'username',
+                            message: 'username already taken'
+                        },
+                    ],
+                };
+            }
+        }
+
+        return {
+            user
+        };
+    }
     }
 }
